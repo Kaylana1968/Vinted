9b826baba855c2563ca0581ed74cbbb7b01eb13d
Vanessa <vanessa.boissierpro@gmail.com>
2024-10-02 10:21:48 +0200

    Ajout article base de donnée et on peut l'afficher sur le home

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Service\CallRequest;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
+
 {
+    public function __construct(private CallRequest $callRequest)
+    {
+        
+    }
     #[Route('/', name: 'home')]
     public function home(): Response
     {
-        return $this->render('main/home.html.twig', [
-            'controller_name' => 'MainController',
+        $allArticle= $this->callRequest->GetAllArticle();
+        return $this -> render('main/home.html.twig',[
+            'all_article'=>$allArticle
         ]);
     }
 
---
+++ src/Service/CallRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+
+
+class CallRequest
+
+{
+    public function __construct(private LoggerInterface $logger ,private readonly EntityManagerInterface
+    $entityManager) {
+       
+    }
+    public function GetAllArticle (){
+
+        $articleList = $this->entityManager->getRepository(Article::class);
+        $allArticle = $articleList->findAll();
+        
+        return $allArticle;
+    }
+}
